a4473daa77c53dc7e021be029eb1eee98ff37b35
Morris Mukiri <morrismukiri@gmail.com>
2022-09-05 17:25:30 +0300

    remove config file
    Signed-off-by: Morris Mukiri <morrismukiri@gmail.com>

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -19,18 +19,18 @@ def test_rapidpro_archiver_app_files(host):
     assert appDir.user == "rapidpro-archiver"
     assert appDir.group == "rapidpro-archiver"
     assert appDir.is_symlink
-    assert appDir.linked_to == "/home/rapidpro-archiver/app-versioned/5.6.0"
+    assert appDir.linked_to == "/home/rapidpro-archiver/app-versioned/7.2.0"
 
-    appVersionedDir = host.file("/home/rapidpro-archiver/app-versioned/5.6.0")
+    appVersionedDir = host.file("/home/rapidpro-archiver/app-versioned/7.2.0")
     assert appVersionedDir.exists
     assert appVersionedDir.user == "rapidpro-archiver"
     assert appVersionedDir.group == "rapidpro-archiver"
     assert appVersionedDir.is_directory
-    assert oct(appVersionedDir.mode) == "0o755"
+    assert oct(appVersionedDir.mode) == "00755"
 
-    configFile = host.file("/home/rapidpro-archiver/app/archiver.toml")
-    assert configFile.exists
-    assert configFile.user == "rapidpro-archiver"
-    assert configFile.group == "rapidpro-archiver"
-    assert configFile.is_file
-    assert oct(configFile.mode) == "0o750"
+    envfile = host.file("/home/rapidpro-archiver/app/environment")
+    assert envfile.exists
+    assert envfile.user == "rapidpro-archiver"
+    assert envfile.group == "rapidpro-archiver"
+    assert envfile.is_file
+    assert oct(envfile.mode) == "00750"
