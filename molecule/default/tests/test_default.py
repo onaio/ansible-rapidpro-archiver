d2513b1f90e40ff9319c4627d3743e5254db98f4
Morris Mukiri <morrismukiri@gmail.com>
2022-09-06 16:25:59 +0300

    use systemd images and use sytemd as init program
    Signed-off-by: Morris Mukiri <morrismukiri@gmail.com>

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,13 +6,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_rapidpro_archiver_service(host):
-    rapidpro_archiver = host.service("rapidpro-archiver")
-
-    assert rapidpro_archiver.is_running
-    assert rapidpro_archiver.is_enabled
-
-
 def test_rapidpro_archiver_app_files(host):
     appDir = host.file("/home/rapidpro-archiver/app")
     assert appDir.exists
@@ -26,11 +19,11 @@ def test_rapidpro_archiver_app_files(host):
     assert appVersionedDir.user == "rapidpro-archiver"
     assert appVersionedDir.group == "rapidpro-archiver"
     assert appVersionedDir.is_directory
-    assert oct(appVersionedDir.mode) == "00755"
+    assert oct(appVersionedDir.mode) == "0o755"
 
     envfile = host.file("/home/rapidpro-archiver/app/environment")
     assert envfile.exists
     assert envfile.user == "rapidpro-archiver"
     assert envfile.group == "rapidpro-archiver"
     assert envfile.is_file
-    assert oct(envfile.mode) == "00750"
+    assert oct(envfile.mode) == "0o750"
