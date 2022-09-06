07fb24f7b470e346e54a52e0c4400fd0f20f8305
Morris Mukiri <morrismukiri@gmail.com>
2022-09-06 17:32:09 +0300

    Use Environment File as Source of Config (#1)
    * update env-vars
    
    Signed-off-by: Morris Mukiri <morrismukiri@gmail.com>
    
    * Use environment file for envvars
    
    Signed-off-by: Morris Mukiri <morrismukiri@gmail.com>
    
    * fix linting issues
    
    Signed-off-by: Morris Mukiri <morrismukiri@gmail.com>
    
    * Update handler name
    
    Signed-off-by: Morris Mukiri <morrismukiri@gmail.com>
    
    * remove config file
    
    Signed-off-by: Morris Mukiri <morrismukiri@gmail.com>
    
    * molecule converge don't archive
    
    Signed-off-by: Morris Mukiri <morrismukiri@gmail.com>
    
    * use systemd images and use sytemd as init program
    
    Signed-off-by: Morris Mukiri <morrismukiri@gmail.com>
    
    Signed-off-by: Morris Mukiri <morrismukiri@gmail.com>

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,31 +6,24 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_rapidpro_archiver_service(host):
-    rapidpro_archiver = host.service("rapidpro-archiver")
-
-    assert rapidpro_archiver.is_running
-    assert rapidpro_archiver.is_enabled
-
-
 def test_rapidpro_archiver_app_files(host):
     appDir = host.file("/home/rapidpro-archiver/app")
     assert appDir.exists
     assert appDir.user == "rapidpro-archiver"
     assert appDir.group == "rapidpro-archiver"
     assert appDir.is_symlink
-    assert appDir.linked_to == "/home/rapidpro-archiver/app-versioned/5.6.0"
+    assert appDir.linked_to == "/home/rapidpro-archiver/app-versioned/7.2.0"
 
-    appVersionedDir = host.file("/home/rapidpro-archiver/app-versioned/5.6.0")
+    appVersionedDir = host.file("/home/rapidpro-archiver/app-versioned/7.2.0")
     assert appVersionedDir.exists
     assert appVersionedDir.user == "rapidpro-archiver"
     assert appVersionedDir.group == "rapidpro-archiver"
     assert appVersionedDir.is_directory
     assert oct(appVersionedDir.mode) == "0o755"
 
-    configFile = host.file("/home/rapidpro-archiver/app/archiver.toml")
-    assert configFile.exists
-    assert configFile.user == "rapidpro-archiver"
-    assert configFile.group == "rapidpro-archiver"
-    assert configFile.is_file
-    assert oct(configFile.mode) == "0o750"
+    envfile = host.file("/home/rapidpro-archiver/app/environment")
+    assert envfile.exists
+    assert envfile.user == "rapidpro-archiver"
+    assert envfile.group == "rapidpro-archiver"
+    assert envfile.is_file
+    assert oct(envfile.mode) == "0o750"
